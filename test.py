723666b20369592fe636a497ff9a0e52b8db42c2
mendesrl <ribeiro.larissamendes@gmail.com>
2023-06-27 19:40:20 -0400

    add bug

--- test.py
+++ test.py
@@ -2,13 +2,13 @@ pais = "Italia"
 ganhou = 4
 print(pais, "ganhou", ganhou)
 
-# import Random
-# a = random.randint(1,12)
-# b = random.randint(1,12)
-# for i in range(10):
-#     question = "What is "+a+" x "+b+"? "
-#     answer = input(question)
-#     if answer = a*b
-#         print (Well done!)
-#     else:
-#         print("No.")
+import Random
+a = random.randint(1,12)
+b = random.randint(1,12)
+for i in range(10):
+    question = "What is "+a+" x "+b+"? "
+    answer = input(question)
+    if answer = a*b
+        print (Well done!)
+    else:
+        print("No.")
