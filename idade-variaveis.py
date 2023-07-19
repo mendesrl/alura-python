9ab5475df19982a66d29e9f09c7359ea50b1b8ad
mendesrl <ribeiro.larissamendes@gmail.com>
2023-07-18 22:01:13 -0400

    feat: add if conditions

---
+++ idade-variaveis.py
@@ -0,0 +1,10 @@
+idade_str = input("Digite sua idade: ")
+idade = int(idade_str)
+
+maior_idade = idade > 18
+crianca     = idade < 12
+adolescente = idade > 12
+
+print(maior_idade)
+print(crianca)
+print(adolescente)
